2dbe5c3348968f2debda6d0cf158c4dc105d7fa3
Dhruv Pal <dhruvpal.4818@gmail.com>
2024-12-30 23:42:17 +0530

    add coincontext

--- src/Context/CoinContext.jsx
+++ src/Context/CoinContext.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
@@ -11,12 +12,44 @@ const CoinContextProvider = (props) => {
   });
 
   const fetchAllCoin = async () => {
-    const options = { 
+    const options = {
       method: "GET",
       headers: {
         accept: "application/json",
         "x-cg-demo-api-key": "CG-PjXXkiyAxjP6BQoQgXa5PoFz",
       },
     };
+
+    try {
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+        options
+      );
+      if (!response.ok) {
+        console.log("Failed to fetch data");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log("Error fetching coin data:", err);
+    }
   };
+
+  useEffect(() => {
+    fetchAllCoin();
+  }, [currency]);
+
+  const contextValue = {
+    allCoin,
+    currency,
+    setCurrency,
+  };
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
 };
+
+export default CoinContextProvider;
