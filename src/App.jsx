c764925dfa70cedbfc55769c3530872cbd484e70
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-01-08 18:42:27 +0530

    update coins path

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,13 @@ export default function App() {
         <div className="circle circle2"></div>
         <div className="circle circle3"></div>
         <div className="circle circle4"></div>
-        <img src="./public/coin1.png" alt="" className="coin1 absolute" />
-        <img src="./public/coin2.png" alt="" className="coin2 absolute" />
-        <img src="./public/coin3.png" alt="" className="coin3 absolute" />
-        <img src="./public/coin4.png" alt="" className="coin4 absolute" />
+        <img src="/coin1.png" alt="" className="coin1 absolute" />
+        <img src="/coin2.png" alt="" className="coin2 absolute" />
+        <img src="/coin3.png" alt="" className="coin3 absolute" />
+        <img src="/coin4.png" alt="" className="coin4 absolute" />
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} /> 
           <Route path="/coin/:coinId" element={<Coin />} />
         </Routes>
         <Footer />
